70f05a47b9de5677ee918c80f13d23321f384074
FerdiBoy01 <frdiboy.01@gmail.com>
2026-04-21 17:29:29 +0700

    update

--- job-ai-backend/src/app.js
+++ job-ai-backend/src/app.js
@@ -5,7 +5,7 @@ const apiRoutes = require('./routes/api');
 const app = express();
 
 // Middleware
-app.use(cors({ origin: '*'}));
+app.use(cors());
 app.use(express.json()); // Agar bisa membaca req.body berupa JSON
 
 // Routes
